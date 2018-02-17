114d9d91d893d885b5c18fe0b6b2380f4de59a5d
vini <vini@gmail.com>
2018-02-17 17:38:54 -0200

    fix

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,12 +1,11 @@
 angular.module('starter.controllers', [])
 
-.controller('FormularioCtrl', function($scope,$state,$rootScope,$q,$timeout,$ionicLoading,$ionicPopup) {
+.controller('FormularioCtrl', function($scope,$state,$rootScope,$q,$timeout,$ionicLoading,$ionicPopup) {  
   
+  $scope.voltar = function(){
+      $state.go("inicial");
+  };
   
-
-  
-
- 
   $scope.enviarPagamento = function(){
     $scope.valor = document.querySelector("input").value;
     if($scope.valor != "" && $scope.valor != 0 && $scope.valor > 0){
@@ -37,54 +36,53 @@ angular.module('starter.controllers', [])
   }
 
   $rootScope.scanQR = function(){
-     $scope.carregandoQR = false;
      $ionicLoading.show();
 
      var defer = $q.defer();
      if(window.cordova){
-        $timeout(function() {
-            cordova.plugins.barcodeScanner.scan(
-              function (result) {
-                  if(result.text == ""){
-                    $ionicLoading.hide();
-                    defer.reject(true);
-                  }
+      cordova.plugins.barcodeScanner.scan(
+        function (result) {
+            if(result.text == ""){
+              $ionicLoading.hide();
+              defer.reject(true);
+            }
 
-                  $rootScope.qr = result.text;
-                  $rootScope.qr = result.format;
-                  
-                  console.log("We got a barcode\n" +
-                          "Result: " + result.text + "\n" +
-                          "Format: " + result.format + "\n" +
-                          "Cancelled: " + result.cancelled);
+            $rootScope.qr = result.text;
+            $rootScope.qr = result.format;
+            
+            console.log("We got a barcode\n" +
+                    "Result: " + result.text + "\n" +
+                    "Format: " + result.format + "\n" +
+                    "Cancelled: " + result.cancelled);
 
-                   $ionicLoading.hide();
+             $ionicLoading.hide();
 
-                   defer.resolve(true);
-              },
-              function (error) {
-                  console.log("Scanning failed: " + error);
-                  $ionicLoading.hide();
-                  defer.reject(true);
-              },
-              {
-                  preferFrontCamera : false, // iOS and Android
-                  showFlipCameraButton : false, // iOS and Android
-                  showTorchButton : false, // iOS and Android
-                  torchOn: false, // Android, launch with the torch switched on (if available)
-                  saveHistory: false, // Android, save scan history (default false)
-                  prompt : "Coloque o QR para ser lido", // Android
-                  resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-                  formats : "QR_CODE", // default: all but PDF_417 and RSS_EXPANDED
-                  orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
-                  disableAnimations : true, // iOS
-                  disableSuccessBeep: false // iOS and Android
-              }       );
-        },1000,true);
-         
-
+             defer.resolve(true);
+        },
+        function (error) {
+        
+            console.log("Scanning failed: " + error);
+            $ionicLoading.hide();
+            defer.reject(true);
+        
+        },
+        {
+        
+            preferFrontCamera : false, // iOS and Android
+            showFlipCameraButton : false, // iOS and Android
+            showTorchButton : false, // iOS and Android
+            torchOn: false, // Android, launch with the torch switched on (if available)
+            saveHistory: false, // Android, save scan history (default false)
+            prompt : "Coloque o QR para ser lido", // Android
+            resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+            formats : "QR_CODE", // default: all but PDF_417 and RSS_EXPANDED
+            orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
+            disableAnimations : true, // iOS
+            disableSuccessBeep: false // iOS and Android
+        
+        });
+       
      }else{
-       $scope.carregandoQR = true;
        $ionicLoading.hide();
        defer.resolve(true);
      }
